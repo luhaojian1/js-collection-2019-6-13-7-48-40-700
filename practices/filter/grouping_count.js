ecb1a549501d4927199449ab2917a07381b64ac4
LUAL6 <962671114@qq.com>
2019-07-10 15:55:07 +0800

    finished grouping_count

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,13 @@
 'use strict';
 
 function grouping_count(collection) {
-
-  //implement here
+  let result = {};
+  collection.filter((number) =>{
+    result[number] = result[number] >= 1 ? result[number] + 1 : 1;
+    console.log(result);
+    return true;
+  })
+  return result;
 }
 
 module.exports = grouping_count;
