f3c1d292c322221eb5158f47f7cd3c50ad617581
LUAL6 <962671114@qq.com>
2019-07-10 15:15:53 +0800

    finish practices/map/map_to_even.js

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,5 +1,6 @@
 'use strict';
 function map_to_even(collection){
-  return [2,4,6,8,10];
+  const arr = collection.map(x => x * 2);
+  return arr;
 }
 module.exports = map_to_even;
--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,6 +1,7 @@
 'use strict';
 var map_to_four_multiples_add_one = function(collection){
-  return [5,9,13,17,21];
+  const arr = collection.map(x => x*4+1 );
+  return arr;
 };
 
 module.exports = map_to_four_multiples_add_one;
